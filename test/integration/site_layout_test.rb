dfe8e70c339119bbafda16ec9c12fbfafa368bdd
Yeqing Liu <40677349+PeasOfMind@users.noreply.github.com>
2019-09-15 15:27:43 -0400

    Add new user route and tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,7 +9,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+
+  test "contact page title" do
     get contact_path
     assert_select "title", full_title("Contact")
   end
+
+  test "signup page title" do
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+
 end
